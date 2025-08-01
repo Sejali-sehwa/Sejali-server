c87396bc4156455219279c41a4ae871c43495995
Knhye <knahye311@gmail.com>
2025-08-01 11:48:01 +0900

    패키지명 명시

--- src/main/java/nahye/sejali/SejaliApplication.java
+++ src/main/java/nahye/sejali/SejaliApplication.java
@@ -2,8 +2,12 @@ package nahye.sejali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "nahye.sejali.repository")
+@EntityScan(basePackages = "nahye.sejali.entity")
 public class SejaliApplication {
 
 	public static void main(String[] args) {
